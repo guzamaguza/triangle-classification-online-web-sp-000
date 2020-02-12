e62b8ed7d015a346792ee58b9038b1cac3e9ae75
Mark A Guza <guzamaguza@gmail.com>
2020-02-12 02:37:59 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
     def kind
       if @first == @second && @second == @third
         self.kind = equilateral
-      elsif @first == @second ^ @first == @third ^ @second == @third 
+      elsif @first == @second ^ @first == @third ^ @second == @third
         self.kind = isosceles
       elsif
         self.kind = scalene
