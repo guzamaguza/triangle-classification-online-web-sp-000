526ffff5b9cc155c660546ff87ce0ecc8888d40b
Mark A Guza <guzamaguza@gmail.com>
2020-02-12 02:31:24 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
 class Triangle
-  # write code here
+    attr_accessor :equilateral, :isosceles, :scalene
+
+    def initialize(first,second,third)
+      @first = first 
+      @second = second
+      @third = third 
+    end
+    
+    def kind 
+          
+    end
+
+    def TriangleError(triangle) 
+      if triangle.class != Triangle 
+        begin 
+          raise TriangleError 
+        rescue 
+
+        end
+      end
+        class TriangleError < StandardError
+            
+        end
+     end
 end
